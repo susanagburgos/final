d2ecfb725440a29d3a8a3ef9a45c22cfd59fc913
Susie Gomez <sugo4237@colorado.edu>
2017-05-01 12:13:09 -0600

    updates to site

---
+++ server.js
@@ -0,0 +1,107 @@
+// HTTP PORTION
+
+var http = require('http');
+var fs = require('fs');
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	// console.log("The Request is: " + parsedUrl.pathname);
+		
+	fs.readFile(__dirname + parsedUrl.pathname, 
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			res.writeHead(200);
+			res.end(data);
+  		}
+  	); 	
+}
+
+// WEBSOCKET PORTION
+
+var currentPlayer = 1; 
+var currentRoom = Math.random().toString(36).substring(7); 
+
+var io = require('socket.io').listen(httpServer);
+
+function genShapes(theRoom){
+			var numbersX=[32];
+			var numbersY=[32];
+
+			for(var i=0; i<32; i++){
+				numbersX[i]= Math.random();
+				numbersY[i]= Math.random();
+			}
+
+			io.to(theRoom).emit('getShapes', {"x": numbersX, "y": numbersY}); 
+			console.log("sending shapes");
+
+}
+
+io.sockets.on('connection', 
+
+	function (socket) {
+		var thisRoom = currentRoom;
+
+		// creating a new room
+ 		socket.join(currentRoom);
+
+		console.log("We have a new client: " + socket.id);
+
+		var playerNumber = currentPlayer;
+
+		// updating server's information
+		socket.emit('playerNumber', playerNumber); 
+
+		if(currentPlayer == 4){
+			genShapes(thisRoom);
+		}
+
+		currentPlayer += 1;  
+
+		// creating a new room once more than 4 people are connected
+		if (currentPlayer > 4) {
+			currentPlayer = 1; 
+			// creating random string to name different rooms
+			currentRoom = Math.random().toString(36).substring(7); 
+			console.log('WELCOME TO CURRENT ROOM' + currentRoom);
+		}
+
+		socket.on('recycleColor', function(data){
+				var numberX; 
+				var numberY;
+				numberX= Math.random();
+				numberY= Math.random();
+				io.to(thisRoom).emit('getRecycle', {"x": numberX, "y":numberY, "player": data});
+		});
+
+		// room functionalities here 
+
+		socket.on('shapeChange', function(data) {
+			// what serveer does when it gets message 
+			// filtering data it to the right room
+
+			// everytime somebody changes shape 
+			// client sends change to everyone in room through server
+			io.to(thisRoom).emit('getChange', data); 
+			// console.log("this is what data is: " + data.player);
+			// client will recieve its own changes
+			// taking data from clients and sending it to every client on that room
+		});
+
+		socket.on('gotShape', function(num) {
+			// sending the number of the shape
+			io.to(thisRoom).emit('sendShape', num); 
+		});
+
+		socket.on('disconnect', function() {
+			console.log("Client has disconnected " + socket.id);
+		});
+	}
+);
---
+++ sketch.js
@@ -0,0 +1,233 @@
+// P5 STUFF
+var shapes=[];
+var colors=['blue', 'red', 'green', 'yellow'];
+var playerNumber; 
+var locY;
+var locX;
+var playerCount = 1;
+var scoreArray= [8, 8, 8, 8];
+var int;
+
+
+
+function setup() {
+	locX = [62.5, 187.5, 312.5, 437.5];
+    locY = [575, 575, 575, 575];  
+	createCanvas(500,600);
+}
+
+function draw() {
+	background("white");
+	// create some kind of while loop to go through array
+	sendChange(); 
+
+	for (var i = 0; i < playerCount; i++) {
+		// console.log('draw players'+ i);
+		stroke("black");
+		strokeWeight(3);
+		fill(colors[i]);
+		ellipse(locX[i], locY[i], 30);
+	
+	}
+
+	if(keyIsDown(LEFT_ARROW)){
+		locX[playerNumber-1] = (locX[playerNumber-1] - 3);
+
+		if(locX[playerNumber-1] < 30) {
+			locX[playerNumber-1] = 30; 
+			// user will turn the other way around at same speed
+		}
+	} 
+	if(keyIsDown(RIGHT_ARROW)){
+		locX[playerNumber-1] = (locX[playerNumber-1]+ 3);
+
+		if(locX[playerNumber-1] > 470) {
+			locX[playerNumber-1] = 470; 
+			// user will turn the other way around at same speed
+		}
+	}
+	if(keyIsDown(UP_ARROW)){
+		locY[playerNumber-1] = (locY[playerNumber-1] - 3);
+	}
+	if(keyIsDown(DOWN_ARROW)){
+		locY[playerNumber-1] = (locY[playerNumber-1] + 3);
+	}
+
+	for(var i=0; i< shapes.length; i++){
+		fill(colors[i%4]);
+		shapes[i].display();
+		shapes[i].check(i%4, i);
+		// i is the index of the shape
+		// i%4 give the color of the shape
+	}
+}
+
+function init(){
+
+}
+
+function getPlayerNumber(data){
+	playerNumber = data;
+	alert('You are player ' + colors[playerNumber-1] + '. Eat as many shapes of the same color!');
+	// textSize(32);
+	// text("You are the " + colors[playerCount-1]+ "player");
+	// fill(0, 102, 153);
+}
+
+// receiving change from index
+function receiveChange(data) {
+	// console.log("get change from" + data.player);
+	// client is playerNumber and data.player is the client who's sending data
+
+	if (playerCount < data.player) {
+		playerCount = data.player; 
+		// assumes there is no player #4 if there isn't a player #1etc... 
+	}
+
+	if (playerNumber != data.player) {
+		locX[data.player-1] = data.x;  
+		locY[data.player-1] = data.y;  
+	}
+}
+
+function sendChange() {
+	// sending location for player
+	socket.emit('shapeChange', {"player": playerNumber, "x": locX[playerNumber-1], "y": locY[playerNumber-1]}); 
+}
+
+function recieveShapes(data){
+	for(var i=0; i< 16; i++){
+		shapes.push({
+			"x": data.x[i] * 475,
+			"y": data.y[i] * 475 + 30,
+			"isActive": true,
+			"display": function(){
+				if(this.isActive){
+					noStroke();
+					triangle(this.x, this.y, this.x-20, this.y-30, this.x+20, this.y-30);
+				}
+			},
+			"check": function(color, num){
+				if(this.isActive){
+					var playa = playerNumber-1; 
+					if(dist(locX[playa], locY[playa], this.x, this.y-15) < 32){
+						if(color == playa){
+							this.isActive = false;
+							subtract(playa, num);
+						}
+						else {
+							recycleColor(playa);
+							locY[playa] = 575;
+						}
+
+					}
+				}
+			}
+			});
+		}
+		for(var i=0; i<16; i++) {
+		shapes.push({
+			"x": data.x[i+16] * 475,
+			"y": data.y[i+16] * 475,
+			"isActive": true,
+			"display": function(){
+				if(this.isActive){
+					noStroke();
+					rect(this.x, this.y, 30, 30);
+				}
+			},
+			"check": function(color, num){
+				if(this.isActive){
+					var playa = playerNumber-1; 
+					if(dist(locX[playa], locY[playa], this.x+15, this.y+15) < 35){
+						if(color == playa){
+							this.isActive = false;
+							subtract(playa, num);
+						}
+						else {
+							recycleColor(playa);
+							locY[playa] = 575;
+						}
+
+					}
+				}
+			}
+
+		});
+	}
+}
+
+function hideShape(num) {
+	if (!(num%4 == playerNumber-1)) {
+		shapes[num].isActive = false; 
+		var i = num%4;
+		scoreArray[i]--;
+		console.log( "player: " + (i+1) + " score subtract:" + scoreArray[i]);
+
+		if(scoreArray[i] == 0) {
+			alert(colors[i] + " won!");	
+		}
+	}
+}
+
+function recycleColor(color){
+	socket.emit("recycleColor", playerNumber-1);
+
+	// I want to add a point to the scoreArray 
+	// for each player, but everytime I bump into 
+	// a color that's not mine, it gives me full points
+	// therefore sometimes even though there are no more
+	// shapes for me to eat, I still can't ever reach 0
+	// whoever reaches 0 wins
+	// also when we have more than one player
+	// it adds points on one side but not on other
+
+	// add(int); lit fixed it by adding an if statement
+}
+
+function drawNewColor(data){
+	add(data.player);
+
+	for(var i=data.player; i<32; i+=4 ){
+		if( !shapes[i].isActive){
+
+			if (i/16 < 1) { // we have a triangle
+				shapes[i].isActive = true;
+				shapes[i].x = data.x * 475;
+				shapes[i].y = data.y * 475 + 30;
+				return false; // when we restore one shape, it leaves this fct
+			} 
+
+			shapes[i].isActive = true;
+			shapes[i].x = data.x * 475;
+			shapes[i].y = data.y * 475;
+			return false;
+		}
+	}
+}
+
+function add(i){
+	if(scoreArray[i] < 8){
+		scoreArray[i]++;
+		console.log("player: " + (i+1) + " score add:" + scoreArray[i]);
+	}
+		// scoreArray[i] = scoreArray[i];
+		// console.log('if add');
+		console.log("score in our scoreArrays" + scoreArray[i]);
+		// console.log("player: " + (i+1) + " score add:" + scoreArray[i]);
+}
+
+function subtract(i, num){
+	scoreArray[i]--;
+
+	console.log( "player: " + (i+1) + " score subtract:" + scoreArray[i]);
+
+	socket.emit('gotShape', num); 
+
+	if(scoreArray[i] == 0) {
+		alert(colors[i] + " won!");	
+	}
+}
+
+
+window.addEventListener("load", init);
